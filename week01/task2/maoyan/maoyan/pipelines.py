c5e8574007cbe09858cb08e3643e6aace1738688
xiaozhuangqing <xiaozhuangqing@163.com>
2020-08-26 19:10:51 +0800

    第二次提交

--- week01/task2/maoyan/maoyan/pipelines.py
+++ week01/task2/maoyan/maoyan/pipelines.py
@@ -9,13 +9,14 @@ from itemadapter import ItemAdapter
 
 
 class MaoyanPipeline:
-    def process_item(self, item, spider):
-        movie_name = item['movie_name']
-	movie_types = item['movie_types']
-	movie_release = item['movie_release']
+	def process_item(self,item,spider):
+		movie_name = item['movie_name']
+		movie_types = item['movie_types']
+		movie_release = item['movie_release']
 
-	output = f'|{movie_name}|\t|{movie_types}|\t|{movie_release}|\n\n'
-	with open('./doubanmovie.txt','a+',encoding='utf-8') as article:
-	    article.write(output)
-	
-	return item
+		output = f'|{movie_name}|\t|{movie_types}|\t|{movie_release}|\n\n'
+		with open('./maoyanmovie.txt','a+',encoding='utf-8') as article:
+			article.write(output)	
+
+		return item
+ 
